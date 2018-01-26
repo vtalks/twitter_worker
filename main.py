7b15c28186f310636130a0d2721a14f4a7bc0c9c
Raül Pérez <repejota@gmail.com>
2018-01-26 23:47:58 +0100

    jobs

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 import sys
 
+import schedule
 import requests
 
 
-def main(argv):
+def tweet_content(talk_json):
+    content = ""
+    content += talk_json["title"]
+    content += " "
+    content += "https://vtalks.net/talk/{}".format(talk_json['slug'])
+    return content
+
+
+def job():
     # get a random talk
     r = requests.get('https://vtalks.net/api/random-talk')
+    if r.status_code != 200:
+        print("Can't fetch a random talk, response status code is",
+              r.status_code)
+        exit(1)
 
-    print(r.json())
+    talk_json = r.json()
+
+    content = tweet_content(talk_json)
+
+    print("TWEET:", content)
+
+
+def main(argv):
+    print('Get a random talk ...')
+
+    schedule.every().hours(6).do(job)
+
+    print("Tweeted.")
 
 
 if __name__ == "__main__":
