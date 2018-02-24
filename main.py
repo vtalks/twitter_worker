f77062d205271ded5a2f9a466a4e45b9b7523d80
Raül Pérez <repejota@gmail.com>
2018-02-24 15:11:45 +0100

    Tests on their own module

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 import sys
-import time
-import logging
-
-import schedule
-
-from worker import get_random_talk
-from worker import generate_tweet_content
-from worker import post_tweet
-
-
-def job():
-    talk_json = get_random_talk()
-    if not talk_json:
-        return
-    tweet_length,  tweet_content = generate_tweet_content(talk_json)
-    status = post_tweet(tweet_content)
-    logging.debug(status)
-
-
-def main(argv):
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info('Starting twitter-worker ...')
-    job()
-    schedule.every(6).hours.do(job)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+import worker
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    worker.main(sys.argv[1:])
--- worker.py
+++ worker.py
@@ -1,10 +1,33 @@
 import os
 import requests
-import logging
 import tweepy
 from slugify import slugify
 
+import time
+import logging
+
+import schedule
+
 TWITTER_TWEET_LIMIT = 140
+
+
+def job():
+    talk_json = get_random_talk()
+    if not talk_json:
+        return
+    tweet_length,  tweet_content = generate_tweet_content(talk_json)
+    status = post_tweet(tweet_content)
+    logging.debug(status)
+
+
+def main(argv):
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info('Starting twitter-worker ...')
+    job()
+    schedule.every(6).hours.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 def post_tweet(tweet_content):
