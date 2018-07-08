2a049817b8db5e8357075d101efcd2adea2cc2d2
Raül Pérez <repejota@gmail.com>
2018-07-08 17:38:58 +0200

    Fix API url endpoint

--- settings.py
+++ settings.py
@@ -7,4 +7,4 @@ TLKSIO_TWITTER_SECRET = os.environ.get('TLKSIO_TWITTER_SECRET', "")
 TLKSIO_TWITTER_ACCESS_TOKEN = os.environ.get('TLKSIO_TWITTER_ACCESS_TOKEN', "")
 TLKSIO_TWITTER_ACCESS_SECRET = os.environ.get('TLKSIO_TWITTER_ACCESS_SECRET', "")
 
-VTALKS_API_RANDOM_TALK_URL = 'https://vtalks.net/api/random-talk/'
+VTALKS_API_RANDOM_TALK_URL = 'https://vtalks.net/api/talk/random-talk/'
